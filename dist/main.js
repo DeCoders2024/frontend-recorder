a95d8b4cd367d85c5f4b52dd8fe9123e7b8a9bd1
Ujjwal-Gupta-web <ujjgpta9@gmail.com>
2023-02-12 07:23:26 +0530

    extension

--- dist/main.js
+++ dist/main.js
@@ -1,14 +1,16 @@
 let stream = null,
+	uploadStream=null,
 	audio = null,
 	mixedStream = null,
-	chunks = [], 
+	chunks = [],
 	recorder = null
-	startButton = null,
-	stopButton = null,
-	downloadButton = null,
+startBtn = null,
+	stopBtn = null,
+	downloadBtn = null,
 	recordedVideo = null;
+	uploadBtn = null;
 
-async function setupStream () {
+async function setupStream() {
 	try {
 		stream = await navigator.mediaDevices.getDisplayMedia({
 			video: true,
@@ -39,7 +41,7 @@ function setupVideoFeedback() {
 	}
 }
 
-async function startRecording () {
+async function startRecording() {
 	await setupStream();
 
 	if (stream && audio) {
@@ -48,57 +50,73 @@ async function startRecording () {
 		recorder.ondataavailable = handleDataAvailable;
 		recorder.onstop = handleStop;
 		recorder.start(1000);
-	
-		startButton.disabled = true;
-		stopButton.disabled = false;
-	
+
+		startBtn.disabled = true;
+		stopBtn.disabled = false;
+
 		console.log('Recording started');
 	} else {
 		console.warn('No stream available.');
 	}
 }
 
-function stopRecording () {
+function stopRecording() {
 	recorder.stop();
 
-	startButton.disabled = false;
-	stopButton.disabled = true;
+	startBtn.disabled = false;
+	stopBtn.disabled = true;
 }
 
-function handleDataAvailable (e) {
+function handleDataAvailable(e) {
 	chunks.push(e.data);
 }
 
-function handleStop (e) {
-	const blob = new Blob(chunks, { 'type' : 'video/mp4' });
-	chunks = [];
+function handleStop(e) {
+	uploadStream=e.target.stream;
+	// const blob = new Blob(chunks, { 'type': 'video/mp4' });
+	// chunks = [];
 
-	downloadButton.href = URL.createObjectURL(blob);
-	downloadButton.download = 'video.mp4';
-	downloadButton.disabled = false;
+	// // downloadBtn.href = URL.createObjectURL(blob);
 
-	recordedVideo.src = URL.createObjectURL(blob);
-	recordedVideo.load();
-	recordedVideo.onloadeddata = function() {
-		const rc = document.querySelector(".recorded-video-wrap");
-		rc.classList.remove("hidden");
-		rc.scrollIntoView({ behavior: "smooth", block: "start" });
+	// // downloadBtn.download = 'video.mp4';
+	// // downloadBtn.disabled = false;
 
-		recordedVideo.play();
-	}
+	// recordedVideo.src = URL.createObjectURL(blob);
+	// recordedVideo.load();
+	// recordedVideo.onloadeddata = function () {
+	// 	rc.classList.remove("hidden");
+	// 	const rc = document.querySelector(".recorded-video-wrap");
+	// 	rc.scrollIntoView({ behavior: "smooth", block: "start" });
+	// // 	recordedVideo.play();
+	// }
 
 	stream.getTracks().forEach((track) => track.stop());
 	audio.getTracks().forEach((track) => track.stop());
-
+	upload();
 	console.log('Recording stopped');
 }
 
 window.addEventListener('load', () => {
-	startButton = document.querySelector('.start-recording');
-	stopButton = document.querySelector('.stop-recording');
-	downloadButton = document.querySelector('.download-video');
+	startBtn = document.querySelector('.start-recording');
+	stopBtn = document.querySelector('.stop-recording');
+	downloadBtn = document.querySelector('.download-video');
 	recordedVideo = document.querySelector('.recorded-video');
 
-	startButton.addEventListener('click', startRecording);
-	stopButton.addEventListener('click', stopRecording);
+	uploadVideo = document.querySelector('.upload-video');
+
+	startBtn.addEventListener('click', startRecording);
+	stopBtn.addEventListener('click', stopRecording);
 })
+
+var input = document.querySelector('input[type="file"]')
+
+const upload=()=>{
+	const fileName=prompt("Enter file name");
+	fetch('/', {
+		method: 'POST',
+		body: {upStream: uploadStream, fileName:fileName}
+	  }).then((res)=>{
+		console.log(res)
+	  }).catch((err)=>console.log(err))
+	  alert("You file we be available soon in the notes section")
+}
